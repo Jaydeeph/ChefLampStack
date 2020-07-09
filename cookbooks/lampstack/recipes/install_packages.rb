822987cccffbb218797b1e253daaae10a429bd5f
Jaydeep <jaydeep.hasmukhlal@cognizant.com>
2020-07-09 14:25:05 +0100

    Renamed files to form order. Added missing mariadb service file. Updated cookbook version.

--- cookbooks/lampstack/recipes/install_packages.rb
+++
@@ -1,4 +0,0 @@
-# Installs the following packages. 
-yum_package 'yum install nano curl wget bind-utils telnet install httpd mod_ssl openssl mariadb-server mariadb php php-mysql php-gd php-mbstring' do
-  action :install
-end
