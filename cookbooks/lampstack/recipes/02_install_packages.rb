8eac842adae5b6a04048c02a1293dcf5ba20e1ff
Jaydeep <jaydeep.hasmukhlal@cognizant.com>
2020-07-10 14:57:33 +0100

    Fixed package resource being used wrongly

--- cookbooks/lampstack/recipes/02_install_packages.rb
+++ cookbooks/lampstack/recipes/02_install_packages.rb
@@ -1,4 +1,4 @@
 # Installs the following packages
-package 'nano curl wget bind-utils telnet install httpd mod_ssl openssl mariadb-server mariadb php php-mysql php-gd php-mbstring' do
+package %w(nano curl wget bind-utils telnet install httpd mod_ssl openssl mariadb-server mariadb php php-mysql php-gd php-mbstring) do
   action :install
 end
