13c7c774054233a2c00d20f7bca9ef50aeb2d461
Jaydeep <jaydeep.hasmukhlal@cognizant.com>
2020-07-10 13:56:18 +0100

    Removed error of having yum install in yum package resource

--- cookbooks/lampstack/recipes/02_install_packages.rb
+++ cookbooks/lampstack/recipes/02_install_packages.rb
@@ -1,4 +1,4 @@
 # Installs the following packages
-yum_package 'yum install nano curl wget bind-utils telnet install httpd mod_ssl openssl mariadb-server mariadb php php-mysql php-gd php-mbstring firewalld' do
+yum_package 'nano curl wget bind-utils telnet install httpd mod_ssl openssl mariadb-server mariadb php php-mysql php-gd php-mbstring firewalld' do
   action :install
 end
