6da6b443ca0787314cf10c8aa1c8c90d7ac16ba4
Jaydeep <jaydeep.hasmukhlal@cognizant.com>
2020-07-13 11:14:16 +0100

    using local attribute for package

--- cookbooks/lampstack2/recipes/linux.rb
+++ cookbooks/lampstack2/recipes/linux.rb
@@ -1,6 +1,5 @@
-
-node['lamp']['linux']['package']['list'].each do |p|
-  package p do
+['nano', 'curl', 'wget', 'bind-utils', 'telnet', 'install', 'httpd', 'mod_ssl', 'openssl', 'mariadb-server', 'mariadb', 'php', 'php-mysql', 'php-gd', 'php-mbstring'].each do |package|
+  package package do
     action :install
   end
 end
