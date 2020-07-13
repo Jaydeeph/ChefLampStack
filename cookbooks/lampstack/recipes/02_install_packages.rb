c6d185b483776f12930f2612554a0e2c293bf379
Jaydeep <jaydeep.hasmukhlal@cognizant.com>
2020-07-13 09:15:51 +0100

    changed package install resource

--- cookbooks/lampstack/recipes/02_install_packages.rb
+++ cookbooks/lampstack/recipes/02_install_packages.rb
@@ -1,4 +1,2 @@
 # Installs the following packages
-package %w(nano curl wget bind-utils telnet install httpd mod_ssl openssl mariadb-server mariadb php php-mysql php-gd php-mbstring) do
-  action :install
-end
+package %w(nano curl wget bind-utils telnet install httpd mod_ssl openssl mariadb-server mariadb php php-mysql php-gd php-mbstring)
