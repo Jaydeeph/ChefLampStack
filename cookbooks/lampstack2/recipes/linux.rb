bd8616022ab87387dcc12bc3b6cc240f5a36fa20
Jaydeep <jaydeep.hasmukhlal@cognizant.com>
2020-07-13 11:45:18 +0100

    Fixed mistake made in install package

--- cookbooks/lampstack2/recipes/linux.rb
+++ cookbooks/lampstack2/recipes/linux.rb
@@ -1,4 +1,4 @@
-['nano', 'curl', 'wget', 'bind-utils', 'telnet', 'install', 'httpd', 'mod_ssl', 'openssl', 'mariadb-server', 'mariadb', 'php', 'php-mysql', 'php-gd', 'php-mbstring'].each do |package|
+['nano', 'curl', 'wget', 'bind-utils', 'telnet', 'httpd', 'mod_ssl', 'openssl', 'mariadb-server', 'mariadb', 'php', 'php-mysql', 'php-gd', 'php-mbstring'].each do |package|
   package package do
     action :install
   end
