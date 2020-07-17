f533ad11c663096100afd5395a42966d9ed97cde
Jaydeep <jaydeep.hasmukhlal@cognizant.com>
2020-07-17 18:03:40 +0100

    Created new template for mysql script and run it

--- cookbooks/lampstack2/recipes/mysql.rb
+++ cookbooks/lampstack2/recipes/mysql.rb
@@ -3,33 +3,44 @@ service 'mariadb' do
   action :enable
 end
 
-# Runs the following bash script. The script uses spawn and expect linux script which enables to use mysql_secure_installation for automation
-bash 'secure_mariadb_mysql' do
-  cwd ::Dir.pwd
-  code <<-EOH
-    MYSQL_ROOT_PASSWORD=testing123
-    SECURE_MYSQL=$(expect -c "
-    set timeout 10
-    spawn mysql_secure_installation
-    expect \"Enter current password for root (enter for none):\"
-    send \"$MYSQL\r\"
-    expect \"Change the root password?\"
-    send \"n\r\"
-    expect \"Remove anonymous users?\"
-    send \"y\r\"
-    expect \"Disallow root login remotely?\"
-    send \"y\r\"
-    expect \"Remove test database and access to it?\"
-    send \"y\r\"
-    expect \"Reload privilege tables now?\"
-    send \"y\r\"
-    expect eof
-    ")
-
-    echo "$SECURE_MYSQL"
-    aptitude -y purge expect
-  EOH
+# Places the secure_mysql.sh file in the root user directory".
+template '~/secure_mysql.sh' do
+  mode '0777'
+  source 'secure_mysql.sh.erb'
+  action :create
 end
+
+execute 'apache_configtest' do
+  command '~./secure_mysql.sh \'testing new password\''
+end
+
+# Runs the following bash script. The script uses spawn and expect linux script which enables to use mysql_secure_installation for automation
+# bash 'secure_mariadb_mysql' do
+#   cwd ::Dir.pwd
+#   code <<-EOH
+#     MYSQL_ROOT_PASSWORD=testing123
+#     SECURE_MYSQL=$(expect -c "
+#     set timeout 10
+#     spawn mysql_secure_installation
+#     expect \"Enter current password for root (enter for none):\"
+#     send \"$MYSQL\r\"
+#     expect \"Change the root password?\"
+#     send \"n\r\"
+#     expect \"Remove anonymous users?\"
+#     send \"y\r\"
+#     expect \"Disallow root login remotely?\"
+#     send \"y\r\"
+#     expect \"Remove test database and access to it?\"
+#     send \"y\r\"
+#     expect \"Reload privilege tables now?\"
+#     send \"y\r\"
+#     expect eof
+#     ")
+
+#     echo "$SECURE_MYSQL"
+#     aptitude -y purge expect
+#   EOH
+# end
 
 # Starts httpd service
 service 'mariadb' do
